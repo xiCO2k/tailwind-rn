121e8b168a1b3d8c09c5a0a482be92fa72acd9e3
Francisco Madeira <xico2k@gmail.com>
2020-03-25 12:35:28 +0000

    Change Test Suite to Jest

--- test.js
+++ test.js
@@ -1,28 +1,28 @@
-import test from 'ava';
+/* global test, expect */
 import tailwind, {getColor} from '.';
 
-test('get styles for one class', t => {
-	t.deepEqual(tailwind('text-blue-500'), {color: '#4299e1'});
+test('get styles for one class', () => {
+	expect(tailwind('text-blue-500')).toEqual({color: '#4299e1'});
 });
 
-test('get styles for multiple classes', t => {
-	t.deepEqual(tailwind('text-blue-500 bg-blue-100'), {
+test('get styles for multiple classes', () => {
+	expect(tailwind('text-blue-500 bg-blue-100')).toEqual({
 		color: '#4299e1',
 		backgroundColor: '#ebf8ff'
 	});
 });
 
-test('ignore unknown classes', t => {
-	t.deepEqual(tailwind('text-blue-500 unknown'), {color: '#4299e1'});
+test('ignore unknown classes', () => {
+	expect(tailwind('text-blue-500 unknown')).toEqual({color: '#4299e1'});
 });
 
-test('get color value', t => {
-	t.is(getColor('blue-500'), '#4299e1');
+test('get color value', () => {
+	expect(getColor('blue-500')).toBe('#4299e1');
 });
 
-test('ignore no value param', t => {
-	t.deepEqual(tailwind(null), {});
-	t.deepEqual(tailwind(false), {});
-	t.deepEqual(tailwind(undefined), {});
-	t.deepEqual(tailwind(0), {});
+test('ignore no value param', () => {
+	expect(tailwind(null)).toEqual({});
+	expect(tailwind(false)).toEqual({});
+	expect(tailwind(undefined)).toEqual({});
+	expect(tailwind(0)).toEqual({});
 });
